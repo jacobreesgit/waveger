11257545b37a3746e5f0606b88f9523a323886e0
Your Name <you@example.com>
2025-01-22 19:17:45 +0000

    Almost there

---
+++ frontend/src/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // This ensures that the HTML file is also included
+    "./src/**/*.{js,ts,jsx,tsx,vue}", // This covers all JS/TS/Vue files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
