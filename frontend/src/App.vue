c7968ec8e3a1c9fb37251adad1a4e638bbb33631
jacobreesgit <jacobrees@me.com>
2025-02-02 20:06:36 +0000

    Test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,5 @@
 <template>
-  <div
-    v-show="isBackgroundLoaded"
-    :class="[
-      'app flex flex-col min-h-screen gap-4',
-      { 'fade-in': isBackgroundLoaded },
-    ]"
-  >
+  <div class="app flex flex-col min-h-screen gap-4">
     <!-- Menubar -->
     <Menu class="pt-4 container mx-auto"></Menu>
 
@@ -13,7 +7,7 @@
     <main class="flex-1 container mx-auto flex flex-col pb-4">
       <router-view
         :class="[
-          'flex items-center flex-col mx-auto p-8 gap-4 w-5/6',
+          'flex items-center flex-col mx-auto p-8  gap-4 w-5/6',
           themeClass,
         ]"
       />
@@ -22,7 +16,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from 'vue'
+import { onMounted, computed } from 'vue'
 import { useHot100Store } from './stores/hot100'
 import { useSelectedDateStore } from './stores/selectedDate'
 import Menu from './components/Menu.vue'
@@ -36,31 +30,15 @@ const themeClass = computed(() =>
   isDark.value ? 'glassmorphism-dark' : 'glassmorphism-light'
 )
 
-const isBackgroundLoaded = ref(false)
-
-const preloadImage = (src) => {
-  return new Promise((resolve, reject) => {
-    const img = new Image()
-    img.src = src
-    img.onload = () => resolve(true)
-    img.onerror = reject
-  })
-}
-
 const getFormattedToday = () => new Date().toISOString().split('T')[0]
 
 onMounted(async () => {
   try {
-    // Preload background image
-    await preloadImage('/src/assets/background.jpeg')
-    isBackgroundLoaded.value = true
-
-    // Fetch data after image has loaded
     const today = getFormattedToday()
     selectedDateStore.setSelectedDate(today)
     await hot100Store.fetchHot100(today, '1-10')
   } catch (error) {
-    console.error('Error:', error)
+    console.error('Failed to fetch Hot 100 data:', error)
   }
 })
 </script>
@@ -72,11 +50,9 @@ onMounted(async () => {
   background-position: center;
   background-attachment: fixed;
   filter: contrast(80%) brightness(110%);
-  opacity: 0; /* Initially hidden */
-  transition: opacity 1s ease-in-out; /* Smooth fade-in */
-}
 
-.app.fade-in {
-  opacity: 1; /* Fade in when loaded */
+  @media (max-width: 639px) {
+    padding: 0 1rem 1rem 1rem;
+  }
 }
 </style>
