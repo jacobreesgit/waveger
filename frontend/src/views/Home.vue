3a81efeaa76879dabbeace34fe803ec175195dc1
jacobreesgit <jacobrees@me.com>
2025-02-02 20:14:15 +0000

    Test

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <Heading>Welcome</Heading>
+    {{ userStore }}
   </div>
 </template>
 
 <script setup>
 import Heading from '../components/Heading.vue'
+
+import { useUserStore } from '../stores/users'
+const userStore = useUserStore()
 </script>
 
 <style scoped></style>
